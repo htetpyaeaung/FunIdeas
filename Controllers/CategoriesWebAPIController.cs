e68f405867fd531a638889469b1408ea07ff516c
htetpyaeaung <31040600+htetpyaeaung@users.noreply.github.com>
2020-05-06 21:53:09 +0930

    Add Controller Files

---
+++ Controllers/CategoriesWebAPIController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Assignment1.Net.Models;
+
+namespace Assignment1.Net.Controllers
+{
+    public class CategoriesWebAPIController : ApiController
+    {
+        private FunIdeasEntities db = new FunIdeasEntities();
+
+        // GET: api/CategoriesWebAPI
+        public IQueryable<Category> GetCategories()
+        {
+            return db.Categories;
+        }
+
+        // GET: api/CategoriesWebAPI/5
+        [ResponseType(typeof(Category))]
+        public IHttpActionResult GetCategory(int id)
+        {
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+
+        // PUT: api/CategoriesWebAPI/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCategory(int id, Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != category.categoryID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/CategoriesWebAPI
+        [ResponseType(typeof(Category))]
+        public IHttpActionResult PostCategory(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Categories.Add(category);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (CategoryExists(category.categoryID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = category.categoryID }, category);
+        }
+
+        // DELETE: api/CategoriesWebAPI/5
+        [ResponseType(typeof(Category))]
+        public IHttpActionResult DeleteCategory(int id)
+        {
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            db.Categories.Remove(category);
+            db.SaveChanges();
+
+            return Ok(category);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return db.Categories.Count(e => e.categoryID == id) > 0;
+        }
+    }
+}
---
+++ Controllers/IdeasController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Assignment1.Net.Models;
+
+namespace Assignment1.Net.Controllers
+{
+    public class IdeasController : Controller
+    {
+        private FunIdeasEntities db = new FunIdeasEntities();
+
+       
+        // GET: Ideas
+        public ActionResult Index(int? cateID)
+        {
+
+            var ideas = db.Ideas.Include(i => i.Category).Include(i => i.Image);
+            if(cateID.HasValue)
+            {
+                ideas = ideas.Where(c => c.categoryID == cateID);
+            }
+            return View(ideas.ToList());
+        }
+
+       public ActionResult NavigateDetail(int? id)
+        {
+            Response.Redirect(@"~/FunDetails.aspx?ideaID="+ id);
+            return new EmptyResult();
+        }
+
+        public ActionResult NavigateModel()
+        {
+            Response.Redirect(@"~/IsolationW1.aspx");
+            return new EmptyResult();
+        }
+
+        public ActionResult NavigateTraditional()
+        {
+            Response.Redirect(@"~/IsolationW2.aspx");
+            return new EmptyResult();
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+    }
+}
