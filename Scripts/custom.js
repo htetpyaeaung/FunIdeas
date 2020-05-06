f1015695ef1f8bdd47fbb1be02064f426069fe6c
htetpyaeaung <31040600+htetpyaeaung@users.noreply.github.com>
2020-05-06 22:16:43 +0930

    Script Files

---
+++ Scripts/custom.js
@@ -0,0 +1,10 @@
+﻿var navbar = document.querySelector('nav')
+
+window.onscroll = function () {
+    if (window.pageYOffset > 0) {
+        navbar.classList.add('scrolled')
+    }
+    else {
+        navbar.classList.remove('scrolled')
+    }
+}
